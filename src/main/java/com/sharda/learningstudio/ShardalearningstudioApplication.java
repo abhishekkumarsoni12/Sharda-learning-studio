5d601cf764b984bb51bf5f818b20fe86587e1aba
Abhishek <Abhishek@DESKTOP-02F58QT>
2025-07-22 13:35:29 +0530

    final commit

---
+++ src/main/java/com/sharda/learningstudio/ShardalearningstudioApplication.java
@@ -0,0 +1,13 @@
+package com.sharda.learningstudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardalearningstudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShardalearningstudioApplication.class, args);
+	} 
+
+}
---
+++ src/test/java/com/sharda/learningstudio/ShardalearningstudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharda.learningstudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShardalearningstudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
